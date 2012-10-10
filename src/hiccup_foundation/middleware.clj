08956cfc517877b8ff87ee4d0739c39d760696f6
Michael Drogalis <mjd3089@rit.edu>
2012-10-09 21:31:16 -0400

    Change ns.

---
+++ src/hiccup_foundation/middleware.clj
@@ -0,0 +1,11 @@
+(ns hiccup-foundation.middleware
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]))
+
+(defn wrap-foundation-resources
+  "Add Foundation resources to the handler."
+  [handler]
+  (routes
+   (route/resources "/foundation" {:root "foundation/public"})
+   handler))
+
---
+++ src/hiccup_foundation/page.clj
@@ -0,0 +1,30 @@
+(ns hiccup-foundation.page
+  (:require [hiccup.page :refer :all]))
+
+(defn include-foundation
+  "Add Foundation CSS and JS resources to the page."
+  []
+  (list
+   (include-css "/foundation/css/app.css")
+   (include-css "/foundation/css/foundation.css")
+   (include-js "/foundation/js/foundation.min.js")
+   (include-js "/foundation/js/app.js")
+   (include-js "/foundation/js/jquery.foundation.accordion.js")
+   (include-js "/foundation/js/jquery.foundation.alerts.js")
+   (include-js "/foundation/js/jquery.foundation.buttons.js")
+   (include-js "/foundation/js/jquery.foundation.forms.js")
+   (include-js "/foundation/js/jquery.foundation.mediaQueryToggle.js")
+   (include-js "/foundation/js/jquery.foundation.navigation.js")
+   (include-js "/foundation/js/jquery.foundation.orbit.js")
+   (include-js "/foundation/js/jquery.foundation.reveal.js")
+   (include-js "/foundation/js/jquery.foundation.tabs.js")
+   (include-js "/foundation/js/jquery.foundation.tooltips.js")
+   (include-js "/foundation/js/jquery.foundation.topbar.js")
+   (include-js "/foundation/js/jquery.js")
+   (include-js "/foundation/js/jquery.placeholder.js")
+   (include-js "/foundation/js/modernizr.foundation.js")))
+
+(defn fixed-layout
+  "Encase its contents in a layout container."
+  [& content]
+  [:div.container content])
