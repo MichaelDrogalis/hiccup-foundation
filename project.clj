9618163936c9443ea15b59a2c266253d9f57dba4
Michael Drogalis <mjd3089@rit.edu>
2012-10-09 21:25:48 -0400

    Deps.

--- project.clj
+++ project.clj
@@ -1,6 +1,9 @@
 (defproject hiccup-foundation "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Zurb Foundation in Hiccup"
+  :url "https://github.com/MichaelDrogalis/hiccup-foundation"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [compojure "1.1.1"]
+                 [hiccup "1.0.0"]])
+
